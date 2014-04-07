b7105ed5d878f3f65200daead07d085c7a84f5d6
Powpow Shen <powpowfp@gmail.com>
2014-04-07 17:48:21 +0800

    Change to extension from chrome app.

--- start.js
+++ start.js
@@ -1,17 +1,5 @@
-/**
- * Listens for the app launching then creates the window
- *
- */
-chrome.app.runtime.onLaunched.addListener(function() {
-    var screenWidth = screen.availWidth;
-    var screenHeight = screen.availHeight;
-    var width = screenWidth;
-    var height = screenHeight;
-
-    chrome.app.window.create('index.html', {
-        width: width,
-        height: height,
-        left: ((screenWidth - width) / 2) + width / 2,
-        top: (screenHeight - height) / 2
+chrome.browserAction.onClicked.addListener(function(tab) {
+    chrome.tabs.create({
+        url: "index.html"
     });
 });
